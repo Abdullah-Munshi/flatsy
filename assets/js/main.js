8b9595f408b65a031afd0dc1eaf86e88cdb16b24
Mrh Ripon <stackbind@gmail.com>
2023-05-27 15:22:27 +0600

    language button added in header

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,21 @@ if (document.querySelector(".navbar-toggle") !== null) {
     document.querySelector("body").classList.toggle("extra");
   });
 }
+
+
+if (document.getElementById('language_switch') !== null) {
+  const langSwitch = document.getElementById('language_switch');
+  document.getElementById(langSwitch.dataset.value).classList.add('show');
+  langSwitch.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (this.dataset.value == 'en') {
+      this.dataset.value = 'fr';
+      langSwitch.querySelectorAll('button').forEach(each => each.classList.remove('show'));
+      document.getElementById(langSwitch.dataset.value).classList.add('show');
+    } else if (this.dataset.value == 'fr') {
+      this.dataset.value = 'en'
+      langSwitch.querySelectorAll('button').forEach(each => each.classList.remove('show'));
+      document.getElementById(langSwitch.dataset.value).classList.add('show');
+    }
+  })
+}
